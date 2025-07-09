11476ad8943d5683a172d576eef193738deb7f4f
Akshath varma <n.akshath2006@gmail.com>
2025-07-09 12:35:22 +0530

    Update meanmodemedian.py

--- meanmodemedian.py
+++ meanmodemedian.py
@@ -3,7 +3,9 @@ import statistics
 random_num = [random.randint(100,150)for i in range(100)]
 print("Mean :")
 print(statistics.mean(random_num))
+print("\n")
 print("Median :")
 print(statistics.median(random_num))
+print("\n")
 print("Mode :")
 print(statistics.mode(random_num))
